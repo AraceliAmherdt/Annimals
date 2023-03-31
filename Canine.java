997988296071daa7340514fc2246eb857bca4c1f
Ruben Bonive <ruben.bonive@glovoapp.com>
2023-03-31 17:50:26 +0200

    chore: improve Canine class

---
+++ Canine.java
@@ -0,0 +1,8 @@
+public class Canine extends Mammal {
+    public Canine(String species, String feeding, String name, String breed){
+        this.species = species;
+        this.feeding = feeding;
+        this.name = name;
+        this.breed = breed;
+    }
+}
